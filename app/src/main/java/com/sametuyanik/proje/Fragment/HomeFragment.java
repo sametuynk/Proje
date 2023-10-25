e6156c5b8116ee7f72937066764c406e96df7c8c
Samet uyanık <samet.uyanik2756@gmail.com>
2023-10-25 16:20:34 +0300

    İlk Gönderim

--- app/src/main/java/com/sametuyanik/proje/Fragment/HomeFragment.java
+++ app/src/main/java/com/sametuyanik/proje/Fragment/HomeFragment.java
@@ -14,9 +14,19 @@ import com.sametuyanik.proje.R;
 
 public class HomeFragment extends Fragment {
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+
+
+
     }
+
+
 }
+
+
+
--- app/src/main/java/com/sametuyanik/proje/View/anasayfa.java
+++ app/src/main/java/com/sametuyanik/proje/View/anasayfa.java
@@ -5,16 +5,24 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sametuyanik.proje.Adapter.RecyclerViewAdapter;
+import com.sametuyanik.proje.Fragment.FavoriteFragment;
+import com.sametuyanik.proje.Fragment.HomeFragment;
+import com.sametuyanik.proje.Fragment.SearchFragment;
+import com.sametuyanik.proje.Fragment.ShareFragment;
 import com.sametuyanik.proje.Model.FoodModel;
 import com.sametuyanik.proje.Model.Receipts;
 import com.sametuyanik.proje.R;
 import com.sametuyanik.proje.Service.FoodAPI;
+import com.sametuyanik.proje.databinding.ActivityAnasayfaBinding;
 
 import java.util.ArrayList;
 
@@ -32,14 +40,39 @@ public class anasayfa extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerViewAdapter recyclerViewAdapter;
 
+    ActivityAnasayfaBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anasayfa);
+        binding=ActivityAnasayfaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
 
+        binding.bottomNavigationView.setBackground(null);
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.paylas:
+                    replaceFragment(new ShareFragment());
+                    break;
+                case R.id.favoriler:
+                    replaceFragment(new FavoriteFragment());
+                    break;
+                case R.id.anasayfa:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.search_bar:
+                    replaceFragment(new SearchFragment());
+                    break;
+            }
+            return true;
+
+
+        });
 
 
         recyclerView = findViewById(R.id.recyclerView);
@@ -54,13 +87,20 @@ public class anasayfa extends AppCompatActivity {
         loadData();
 
     }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
 
 
     private void loadData() {
 
         FoodAPI foodAPI = retrofit.create(FoodAPI.class);
 
-        Call<Receipts> call = foodAPI.getProducts("100", "27ce4019daf44998a26116bfa08d8dc2");
+        Call<Receipts> call = foodAPI.getProducts("10", "27ce4019daf44998a26116bfa08d8dc2");
 
         call.enqueue(new Callback<Receipts>() {
             @Override
@@ -68,14 +108,12 @@ public class anasayfa extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     Receipts responseList = response.body();
                     foodModels = new ArrayList<>(response.body().receipts);
-
                     //Recyclerview
                     recyclerView.setLayoutManager(new LinearLayoutManager(anasayfa.this));
                     recyclerViewAdapter = new RecyclerViewAdapter(foodModels, anasayfa.this);
                     recyclerView.setAdapter(recyclerViewAdapter);
                 }
             }
-
             @Override
             public void onFailure(Call<Receipts> call, Throwable t) {
                 Toast.makeText(anasayfa.this, "Hata", Toast.LENGTH_SHORT).show();
