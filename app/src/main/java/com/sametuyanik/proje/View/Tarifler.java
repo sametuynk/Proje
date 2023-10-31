0f295ecf9c96fa8f73d4d4d1a4569588e629f814
Samet uyanık <samet.uyanik2756@gmail.com>
2023-10-31 22:11:31 +0300

    İlk Gönderim

--- app/src/main/java/com/sametuyanik/proje/View/Tarifler.java
+++ app/src/main/java/com/sametuyanik/proje/View/Tarifler.java
@@ -1,10 +1,14 @@
 package com.sametuyanik.proje.View;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sametuyanik.proje.Fragment.FavoriteFragment;
 import com.sametuyanik.proje.Model.FoodModel;
 import com.sametuyanik.proje.R;
 import com.sametuyanik.proje.databinding.ActivityTariflerBinding;
@@ -14,6 +18,7 @@ public class Tarifler extends AppCompatActivity {
     ActivityTariflerBinding binding;
     TextView textViewTarif;
 
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +29,30 @@ public class Tarifler extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
         FoodModel m = (FoodModel) b.get("tarif");
-
         textViewTarif = findViewById(R.id.textViewTarif);
-
-
         textViewTarif = new TextView(this);
         textViewTarif.setText(m.getSummary());
-
         binding.getRoot().addView(textViewTarif);
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent=new Intent(Tarifler.this, FavoriteFragment.class);
+                startActivity(intent);
+
+                /* FavoriteFragment favoriteFragment=new FavoriteFragment();
+                FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameLayout,favoriteFragment);
+                transaction.commit();
+
+                 */
+            }
+        });
 
 
 
